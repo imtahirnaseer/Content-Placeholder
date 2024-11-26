9348727555fb1cd5295d11c51e28c70bb9ae3fc5
Tahir Naseer <150379783+imtahirnaseer@users.noreply.github.com>
2024-11-26 13:57:32 +0530

    script.js

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ setTimeout(getData, 2500)
 
 function getData() {
   header.innerHTML =
-    '<img src="" alt="" />'
+    '<img src="" alt="https://github.com/imtahirnaseer/Content-Placeholder/blob/1599b741c1ff248da6d7e6f57055bddfc35095d4/Screenshot%202024-07-28%20001136.png" />'
   title.innerHTML = 'Who is Tahir Naseer'
   excerpt.innerHTML =
     'He is an Cybersecurity & Computer Engineer and passionate about AI and Software Solutions. With key projects like TahirGPT AI Chatbot, Password Generator,Login to TahirGPT, Code-Visualizer Etc..'
   profile_img.innerHTML =
-    '<img src="" alt="" />'
+    '<img src="" alt="https://github.com/imtahirnaseer/Content-Placeholder/blob/1599b741c1ff248da6d7e6f57055bddfc35095d4/IMG_20241121_090800%5B1%5D12326.jpg" />'
   name.innerHTML = 'Tahir Naseer'
   date.innerHTML = '26-11-2024'
 
