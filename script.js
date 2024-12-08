d84d55ddfd6d0f2263a783bb64efa4786365eacb
Tahir Naseer <150379783+imtahirnaseer@users.noreply.github.com>
2024-12-08 18:15:39 +0530

    script.js

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ setTimeout(getData, 2500)
 
 function getData() {
   header.innerHTML =
-    '<img src="" alt="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.starkcloud.com%2Fen%2Fstarkcloud-blog%2Fthe-future-of-software-development&psig=AOvVaw3ky2tsIy4UvrwO4_DJq97B&ust=1732696184021000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKi6rerK-YkDFQAAAAAdAAAAABAE" />'
+    '<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiXZXZy9qEC0Zo_IknnxmIgmPww7rnyXlnnWtRnHDLysLSZPXT8bSmKcQW_9ASR7PciA8rWB0SuBlp8-Q5h0fvUrMqlmny6ZQayN8aZTGqaNxIEuyGf83kATwtX4duRyVexThPWHtEEv3rL8NgC9LfDDH9NwZvQ3JpRaVNHT8ScH3wglTaylA5DGlyR-Njn/s3020/IMG-20241207-WA0011.jpg" alt="Tahir Project" />'
   title.innerHTML = 'Who is Tahir Naseer'
   excerpt.innerHTML =
     'He is an Cybersecurity & Computer Engineer and passionate about AI and Software Solutions. With key projects like TahirGPT AI Chatbot, Password Generator,Login to TahirGPT, Code-Visualizer Etc..'
   profile_img.innerHTML =
-    '<img src="" alt="https://www.instagram.com/imtahirnaseer/p/DCpevH8TidB/" />'
+    '<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhqKiEDJKDx7ffZdB83Ugq_IEXVcKsv6F3aXhP6-_2L9FXOXr5oQ3rM0riHsGtdCV8lNFmziH3HGAEMSRb_EXBhgfldZ293ptb2-xSGp3x2FTD1Ko2XvJEgnkvFBQl53geK72JwXJ3fw1II6k-683dTN1MI0BxGxwlbrawgL3XewmcvusCsh-yLaYQ-YCM4/s4096/file.png" alt="" />'
   name.innerHTML = 'Tahir Naseer'
   date.innerHTML = '26-11-2024'
 
